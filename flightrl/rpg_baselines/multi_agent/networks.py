e1da87dcbdc297f935a8d2125477a11d0dee2917
kangbeenlee <leekangbin126@gmail.com>
2023-11-06 00:04:58 +0900

    Update for test 41

--- flightrl/rpg_baselines/multi_agent/networks.py
+++ flightrl/rpg_baselines/multi_agent/networks.py
@@ -57,13 +57,13 @@ class Critic_MADDPG(nn.Module):
 class Critic_MATD3(nn.Module):
     def __init__(self, args):
         super(Critic_MATD3, self).__init__()
-        self.fc1 = nn.Linear(args.critic_input_dim, args.hidden_dim)
-        self.fc2 = nn.Linear(args.hidden_dim, args.hidden_dim)
-        self.fc3 = nn.Linear(args.hidden_dim, 1)
+        self.fc1 = nn.Linear(args.critic_input_dim, args.critic_hidden_dim)
+        self.fc2 = nn.Linear(args.critic_hidden_dim, args.critic_hidden_dim)
+        self.fc3 = nn.Linear(args.critic_hidden_dim, 1)
 
-        self.fc4 = nn.Linear(args.critic_input_dim, args.hidden_dim)
-        self.fc5 = nn.Linear(args.hidden_dim, args.hidden_dim)
-        self.fc6 = nn.Linear(args.hidden_dim, 1)
+        self.fc4 = nn.Linear(args.critic_input_dim, args.critic_hidden_dim)
+        self.fc5 = nn.Linear(args.critic_hidden_dim, args.critic_hidden_dim)
+        self.fc6 = nn.Linear(args.critic_hidden_dim, 1)
 
         if args.use_orthogonal_init:
             print("------use_orthogonal_init------")
